534320f8e4a23e15a1435b4d5db897ae54244fd9
Kyle Nieber <kyle.nieber@gmail.com>
2017-04-07 21:28:07 -0400

    remove node_modules

--- node_modules/jumpjs/examples/express/middleware/res-locals.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req, res, next) => {
-  res.locals = {
-    title: 'JumpJS'
-  };
-  next();
-};
--- node_modules/jumpjs/src/lib/input/index.js
+++
@@ -1,5 +0,0 @@
-var Keyboard = require('./Keyboard');
-
-module.exports = {
-  Keyboard
-};
